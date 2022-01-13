4863601bd92fac7b20db9e45b183126dc0e65be4
Valdemar696 <valdemar.tigrov@gmail.com>
2022-01-13 19:34:58 +0300

    bruh

--- app/src/main/java/Data/DatabaseHandler.java
+++ app/src/main/java/Data/DatabaseHandler.java
@@ -1,11 +1,17 @@
 package Data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import Model.Car;
 import Utils.Util;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
@@ -33,4 +39,73 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
     //CRUD
     //Create, Read, Update, Delete
+
+    public void addCar(Car car) { //метод добавления машины в БД
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase(); //строка созд-я переменной класса склбд, позволяющий записывать в бд
+
+        ContentValues contentValues = new ContentValues(); //класс кв, позволяющий взаимодействовать с бд
+        contentValues.put(Util.KEY_NAME, car.getName());
+        contentValues.put(Util.KEY_PRICE, car.getPrice());
+
+        sqLiteDatabase.insert(Util.TABLE_NAME, null, contentValues);
+        sqLiteDatabase.close();
+    }
+
+    public Car getCar(int id) {
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+
+        Cursor cursor = sqLiteDatabase.query(Util.TABLE_NAME, new String[] {Util.KEY_ID, Util.KEY_NAME,
+                Util.KEY_PRICE}, Util.KEY_ID + "=?", new String[] {String.valueOf(id)}, null,
+                null, null, null);
+        if (cursor != null) {
+            cursor.moveToFirst();
+        }
+
+        Car car = new Car(Integer.parseInt(cursor.getString(0)), cursor.getString(1), cursor.getString(2));
+        return car;
+    }
+
+    public List<Car> getAllCars() { //List - интерфейс, наследуемый ArrayList'ом. Своего рода абстракция. Поставлен он, т.к. можно вернуть не только ЭррейЛист, но и Stack, например
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+
+        List<Car> carsList = new ArrayList<>();
+
+        String selectAllCars = "SELECT * FROM " + Util.TABLE_NAME;
+        Cursor cursor = sqLiteDatabase.rawQuery(selectAllCars, null);
+        if (cursor.moveToFirst()) {
+            do {
+                Car car = new Car();
+                car.setId(Integer.parseInt(cursor.getString(0)));
+                car.setName(cursor.getString(1));
+                car.setPrice(cursor.getString(2));
+
+                carsList.add(car);
+            } while (cursor.moveToNext());
+        }
+
+        return carsList;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/carsdb/MainActivity.java
+++ app/src/main/java/com/example/carsdb/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.carsdb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
+
+import Data.DatabaseHandler;
+import Model.Car;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DatabaseHandler databaseHandler = new DatabaseHandler(this);
+
+        databaseHandler.addCar(new Car("Toyota", "30 000 $"));
+        databaseHandler.addCar(new Car("BMW", "45 000 $"));
+        databaseHandler.addCar(new Car("Audi", "50 000 $"));
+        databaseHandler.addCar(new Car("Fiat", "35 000 $"));
+
+        List<Car> carList = databaseHandler.getAllCars();
+
+        for (Car car : carList) {
+            Log.i("Car info: ", "ID " + car.getId() + ", name " + car.getName() + ", price " + car.getPrice());
+        }
     }
 }
